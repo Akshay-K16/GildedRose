b6078757b55b9e1cf3663a268363b30bd164c202
Akshay-K16 <akhemaney@gmail.com>
2025-07-03 14:57:24 +0100

    Added tests and refactored main code

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,56 +1,31 @@
 package com.gildedrose
 
 class GildedRose(val items: List<Item>) {
-
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
+            var qualityChange = 1
+            if (items[i].sellIn <= 0) qualityChange *= 2
+            if (items[i].name.length > 8 && items[i].name.substring(0, 8) == "Conjured") qualityChange *= 2
+
+            if (items[i].name == "Aged Brie") {
+                items[i].quality += qualityChange
+            } else if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                when (items[i].sellIn) {
+                    in Int.MIN_VALUE..0 -> items[i].quality = 0
+                    in 1..5 -> items[i].quality += 3
+                    in 6..10 -> items[i].quality += 2
+                    else -> items[i].quality += 1
                 }
+            } else if (items[i].name == "Sulfuras, Hand of Ragnaros") {
+                continue;
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+                items[i].quality -= qualityChange
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
+            items[i].sellIn -= 1
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+            if (items[i].quality < 0) items[i].quality = 0
+            if (items[i].quality > 50) items[i].quality = 50
         }
     }
 
--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,12 +6,233 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
+    fun testUpdateLeavesNameUnchanged() {
         val items = listOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals(items[0].name, app.items[0].name)
+    }
 
+    @Test
+    fun testUpdateDecreasesQualityNormal() {
+        val items = listOf(Item("Elixir of the Mongoose", 10, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(9, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdateDecreasesQualityExpired() {
+        val items = listOf(Item("Elixir of the Mongoose", -5, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(8, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdateDecreasesQualityBorderlineExpired() {
+        val items = listOf(Item("Elixir of the Mongoose", 0, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(8, app.items[0].quality)
+    }
+
+    @Test
+    fun testQualityNeverNegativeNormal() {
+        val items = listOf(Item("Elixir of the Mongoose", 10, 0))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
+    }
+
+    @Test
+    fun testQualityNeverNegativeExpired() {
+        val items = listOf(Item("Elixir of the Mongoose", -5, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdateBrieIncreasesQualityNormal() {
+        val items = listOf(Item("Aged Brie", 10, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(11, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdateBrieIncreasesQualityExpired() {
+        val items = listOf(Item("Aged Brie", -5, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(12, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdateBrieIncreasesQualityBorderlineExpired() {
+        val items = listOf(Item("Aged Brie", 0, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(12, app.items[0].quality)
+    }
+
+    @Test
+    fun testBrieQualityNeverExceedsFiftyNormal() {
+        val items = listOf(Item("Aged Brie", 10, 50))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun testBrieQualityNeverExceedsFiftyExpired() {
+        val items = listOf(Item("Aged Brie", -5, 49))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdatePassIncreasesQualityNormal() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 20, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(11, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdatePassIncreasesQualityTenDays() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(12, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdatePassIncreasesQualityEightDays() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 8, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(12, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdatePassIncreasesQualityFiveDays() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(13, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdatePassIncreasesQualityThreeDays() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 3, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(13, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdatePassExpired() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
+    }
+
+    @Test
+    fun testPassQualityNeverExceedsFiftyNormal() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 20, 50))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun testPassQualityNeverExceedsFiftyTenDays() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 49))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun testPassQualityNeverExceedsFiftyFiveDays() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 48))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdateLeavesSulfurasUnchangedNormal() {
+        val items = listOf(Item("Sulfuras, Hand of Ragnaros", 10, 80))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(items[0].name, app.items[0].name)
+        assertEquals(items[0].quality, app.items[0].quality)
+        assertEquals(items[0].sellIn, app.items[0].sellIn)
+    }
+
+    @Test
+    fun testUpdateLeavesSulfurasUnchangedExpired() {
+        val items = listOf(Item("Sulfuras, Hand of Ragnaros", 0, 80))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(items[0].name, app.items[0].name)
+        assertEquals(items[0].quality, app.items[0].quality)
+        assertEquals(items[0].sellIn, app.items[0].sellIn)
+    }
+
+    @Test
+    fun testUpdateLeavesSulfurasUnchangedBorderlineExpired() {
+        val items = listOf(Item("Sulfuras, Hand of Ragnaros", -10, 80))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(items[0].name, app.items[0].name)
+        assertEquals(items[0].quality, app.items[0].quality)
+        assertEquals(items[0].sellIn, app.items[0].sellIn)
+    }
+
+    @Test
+    fun testUpdateConjuredNormal() {
+        val items = listOf(Item("Conjured Mana Cake", 10, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(8, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdateConjuredBorderlineExpired() {
+        val items = listOf(Item("Conjured Mana Cake", 0, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(6, app.items[0].quality)
+    }
+
+    @Test
+    fun testUpdateConjuredExpired() {
+        val items = listOf(Item("Conjured Mana Cake", -5, 10))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(6, app.items[0].quality)
+    }
+
+    @Test
+    fun testConjuredQualityNeverNegativeNormal() {
+        val items = listOf(Item("Conjured Mana Cake", 10, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
+    }
+
+    @Test
+    fun testConjuredQualityNeverNegativeExpired() {
+        val items = listOf(Item("Conjured Mana Cake", 0, 3))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
     }
 
 }
